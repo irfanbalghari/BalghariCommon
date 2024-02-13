316d6c76073ac481500e680d23ccb2073942c438
irfan.ullah <irfan.ullah@IRFAN-ULLAH>
2024-02-13 20:51:19 +0500

    login and register basic

--- ROW/src/app/app.routes.ts
+++ ROW/src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Modules/Auth/Login/Login.component';
 import { RegisterComponent } from './Modules/Auth/register/register.component';
+import { authGuard } from './Modules/Auth/auth.guard';
 
 export const routes: Routes = [
    { 
-    path: '',
+    path: 'login',
     component: LoginComponent
    },
    { 
    path: 'register',
    component: RegisterComponent 
-   }
+   },
+   {
+      path: 'dashboard',
+      loadChildren: () => import('./Modules/dashboard/dashboard.module').then(m=>m.DashboardModule),
+      canMatch: [authGuard]
+  },
+  {
+   path: '**',
+   component: LoginComponent
+  }
 ];
